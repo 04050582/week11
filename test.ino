e35cd38f6d29b289cc02fa830db545a5b79c2e2c
04050582 <hc920052@gmail.com>
2017-06-19 17:17:55 +0800

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.print("04050582 chou zhi chen");
+  lcd.begin(20, 2);
+  lcd.print("04050582");
+  lcd.setCursor(0,1);
+  lcd.print("chou zhi chen");
+}
+void loop() 
+{
+  if(Serial.available())
+  {
+    delay(100);
+    lcd.clear();
+    while(Serial.available()>0)
+    {
+      lcd.write(Serial.read());
+    }
+  }
+}
